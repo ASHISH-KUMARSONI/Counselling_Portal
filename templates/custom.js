a9eaf32e7957cfea88d6874f2031fe00dacee1a6
ASHISH-KUMARSONI <ashishsoni91717@gmail.com>
2021-07-13 14:54:34 +0530

    A project where a client can ask a counsellor to fix an appointment

---
+++ templates/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   $('.chat_icon').click(function(){
+      $(".chat_box").removeClass("active focus");
+      $(".chat_box").toggleClass('.active');
+    });
+
+ });
